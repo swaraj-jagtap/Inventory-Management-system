1aae3eeff1c9c429f7c9da36c751afc2cc37987f
swaraj-jagtap <swarajjagtap77@gmail.com>
2025-08-15 14:59:01 +0530

    final setup frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import GenericEntityPage from './pages/GenericEntityPage';
 import LoginPage from './pages/LoginPage';
 
 // ✅ Use full backend URL since backend runs on 5001
-const API_URL = 'http://localhost:5001/api';
+const API_URL = process.env.BACKEND_API_URL;
 
 export default function App() {
     const [user, setUser] = useState(null);
